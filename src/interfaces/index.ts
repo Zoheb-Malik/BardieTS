6c2a82fda4704f944a5066579f9d404c0ba43441
Zoheb Malik <mzohebm@gmail.com>
2023-12-27 20:27:37 +0000

    interface: import export
    Makes it easier to read.

---
+++ src/interfaces/index.ts
@@ -0,0 +1,5 @@
+import { AIResponse } from "./AIResponse";
+import { AIResponseData } from "./AIResponseData";
+import { questionOptions } from "./questionOptions";
+
+export type { AIResponse, AIResponseData, questionOptions };
