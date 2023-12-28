a0d6c2d1325adefa0cee8762dc7936c78be21512
Zoheb Malik <mzohebm@gmail.com>
2023-12-28 02:16:04 +0000

    tsup.config.ts: update: name
    See title.

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "tsup";
  
 export default defineConfig({
     format: ["cjs", "esm"],
-    entry: ["./src/lib/BardieTS.ts"],
+    entry: ["./src/lib/bardie-ts.ts"],
     dts: true,
     shims: true,
     skipNodeModulesBundle: true,
